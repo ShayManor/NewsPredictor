46ec67b251ff46bbda69e37b4208bf6bf0ce116a
Shay Manor <shay.manor@gmail.com>
2025-08-08 03:06:33 +0000

    feat: put on GPU

--- src/build/classify_articles.py
+++ src/build/classify_articles.py
@@ -3,14 +3,17 @@
 # 3. Cluster with HDBSCAN min_cluster_size=5 min_samples=2 and manually label
 import os
 
-import hdbscan
+from cuml.cluster  import HDBSCAN
 import numpy as np
-import umap
+import cupy as cp
+import torch
+from cuml.manifold import UMAP
 from bertopic import BERTopic
 from datasets import load_dataset
 import pandas as pd
 from matplotlib import pyplot as plt
 from sentence_transformers import SentenceTransformer
+torch.set_float32_matmul_precision('high')
 
 os.environ['TOKENIZERS_PARALLELISM'] = "True"
 
@@ -31,31 +34,32 @@ timestamps = ds["date"]
 assert (len(docs) == len(timestamps))
 
 print("Encoding docs")
-encoder = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+encoder = SentenceTransformer("sentence-transformers/all-mpnet-base-v2", device="cuda", trust_remote_code=True).half()
 emb = encoder.encode(docs, batch_size=256, show_progress_bar=True, normalize_embeddings=True)
-
+emb_gpu = cp.asarray(emb)
 
 print("Fitting clusters")
-umap_model = umap.UMAP(n_neighbors=20, n_components=15,
+umap_model = UMAP(n_neighbors=20, n_components=15,
                        metric="cosine", min_dist=0.0, random_state=42)
 
-hdbscan_model = hdbscan.HDBSCAN(
+hdbscan_model = HDBSCAN(
     min_cluster_size=8,  # try 6, 8, 10
     min_samples=2,  # 1–5; higher -> more noise, fewer clusters
     metric="euclidean",  # UMAP output is Euclidean
     cluster_selection_epsilon=0.03,  # small epsilon helps attach borderline points
     cluster_selection_method="eom",
     prediction_data=True,
-    gen_min_span_tree=True
+    gen_min_span_tree=True,
+
 )
 topic_model = BERTopic(
     embedding_model=None,  # we pass precomputed embeddings
-    umap_model=umap_model,
+    umap_model=None,
     hdbscan_model=hdbscan_model,
     min_topic_size=8,  # keep aligned with HDBSCAN
     top_n_words=10,
     calculate_probabilities=True,
-    verbose=True
+    verbose=True,
 )
 
 topics, probs = topic_model.fit_transform(docs, embeddings=emb)
