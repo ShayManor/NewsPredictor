4f66b27a3861b8adba621b69e21be345f95915f4
ShayManor <shay.manor@gmail.com>
2025-08-09 01:54:30 +0000

    feat: lower classify_articles memory usage

--- src/build/classify_articles.py
+++ src/build/classify_articles.py
@@ -1,24 +1,26 @@
 # 1. Feed article summaries through all‑MPNet‑base‑v2 for 768d vectors
 # 2. Use datasketch at Jaccard >= 0.8 to remove dupes
-# 3. Cluster with HDBSCAN min_cluster_size=5 min_samples=2 and manually label
+# 3. Cluster with HDBSCAN and label
+# Best run from sweep: {'n_neighbors': 38, 'n_components': 19, 'min_dist': 0.26222776670842407, 'min_c
+# luster_size': 4, 'min_samples': 1, 'cluster_epsilon': 0.19291905969822112, 'min_
+# topic_size': 16, 'reassign_threshold': 0.11078554780289114}
+import json
 import os
 
 from cuml.cluster  import HDBSCAN
 import numpy as np
-import cupy as cp
-import torch
 from cuml.manifold import UMAP
 from bertopic import BERTopic
 from datasets import load_dataset
 import pandas as pd
 from matplotlib import pyplot as plt
 from sentence_transformers import SentenceTransformer
-torch.set_float32_matmul_precision('high')
-
+import optuna
+print("New trial")
 os.environ['TOKENIZERS_PARALLELISM'] = "True"
 
 print("Pulling dataset")
-ds = load_dataset("LogeshChandran/newsroom", split="train[:100000]").to_pandas()
+ds = load_dataset("LogeshChandran/newsroom", split="train").to_pandas()
 ds["date"] = pd.to_datetime(ds["date"], format="%Y%m%d", errors="coerce")
 print("Sorting dates and building window")
 ds = ds.dropna(subset=["date"]).sort_values("date").reset_index(drop=True)
@@ -29,53 +31,86 @@ ds["window_id"] = (ds["date"]
                    .astype("int64") // 10 ** 9)
 
 docs = (ds["title"].fillna("") + " " + ds["summary"].fillna("")).tolist()
-timestamps = ds["date"]
+timestamps = ds["date"].dt.to_pydatetime().tolist()
 
 assert (len(docs) == len(timestamps))
 
 print("Encoding docs")
 encoder = SentenceTransformer("sentence-transformers/all-mpnet-base-v2", device="cuda", trust_remote_code=True).half()
 emb = encoder.encode(docs, batch_size=256, show_progress_bar=True, normalize_embeddings=True)
-emb_gpu = cp.asarray(emb)
 
 print("Fitting clusters")
-umap_model = UMAP(n_neighbors=20, n_components=15,
-                       metric="cosine", min_dist=0.0, random_state=42)
+def fit_clusters(n_neighbors, n_components, min_dist, min_cluster_size, min_samples, cluster_epsilon, min_topic_size, top_n_words, nr_bins, threshold):
+    umap_model = UMAP(n_neighbors=n_neighbors, n_components=n_components,
+                           metric="cosine", min_dist=min_dist, random_state=None, build_algo='nn_descent')
+    print("Mapped Clusters")
 
-hdbscan_model = HDBSCAN(
-    min_cluster_size=8,  # try 6, 8, 10
-    min_samples=2,  # 1–5; higher -> more noise, fewer clusters
-    metric="euclidean",  # UMAP output is Euclidean
-    cluster_selection_epsilon=0.03,  # small epsilon helps attach borderline points
-    cluster_selection_method="eom",
-    prediction_data=True,
-    gen_min_span_tree=True,
+    hdbscan_model = HDBSCAN(
+        min_cluster_size=min_cluster_size,  # try 6, 8, 10
+        min_samples=min_samples,  # 1–5; higher -> more noise, fewer clusters
+        metric="euclidean",  # UMAP output is Euclidean
+        cluster_selection_epsilon=cluster_epsilon,  # small epsilon helps attach borderline points
+        cluster_selection_method="eom",
+        prediction_data=False,
+        gen_min_span_tree=False,
+    )
+    print("Scanned clusters")
+    topic_model = BERTopic(
+        embedding_model=None,  # we pass precomputed embeddings
+        umap_model=umap_model,
+        hdbscan_model=hdbscan_model,
+        min_topic_size=min_topic_size,  # keep aligned with HDBSCAN
+        top_n_words=top_n_words,
+        calculate_probabilities=False,
+        verbose=False,
+    )
 
-)
-topic_model = BERTopic(
-    embedding_model=None,  # we pass precomputed embeddings
-    umap_model=None,
-    hdbscan_model=hdbscan_model,
-    min_topic_size=8,  # keep aligned with HDBSCAN
-    top_n_words=10,
-    calculate_probabilities=True,
-    verbose=True,
-)
+    topics, probs = topic_model.fit_transform(docs, embeddings=emb)
+    print("Fit embeddings")
+    topics = topic_model.reduce_outliers(docs, topics, probabilities=probs, strategy="c-tf-idf", threshold=threshold)
+    topic_model.update_topics(docs, topics=topics)
 
-topics, probs = topic_model.fit_transform(docs, embeddings=emb)
-if probs is not None:
-    probs = np.asarray(probs)
-    best = probs.max(axis=1)
-    best_topic = probs.argmax(axis=1)
-    reassign_mask = (np.array(topics) == -1) & (best >= 0.25)  # try 0.20–0.35
-    topics = np.where(reassign_mask, best_topic, topics).tolist()
-topic_model.reduce_topics(docs, topics=topics, nr_topics=None, similarity_threshold=0.85)
+    topic_model.merge_topics(docs, topics)
+    topic_model.update_topics(docs, topics=topics)
 
-topics_over_time = topic_model.topics_over_time(docs, topics, timestamps, nr_bins=30)
+    topics_over_time = topic_model.topics_over_time(docs=docs, topics=topics, timestamps=timestamps, nr_bins=nr_bins, global_tuning=False)
 
-fig = topic_model.visualize_topics_over_time(topics_over_time)
-print(topics)
-print("=========================================")
-print(topics_over_time)
+    fig = topic_model.visualize_topics_over_time(topics_over_time)
+    with open('topics_array.json', 'w') as f:
+        json.dump(topics, f)
+    print(topics)
+    print("=========================================")
+    print(topics_over_time)
+    return topics.count(-1)
+fit_clusters(38, 19, 0.26222776670842407, 4, 1, 0.19291905969822112, 16, 10, threshold=0.11078554780289114, nr_bins=30)
 
+def objective(trial):
+    n_neighbors       = trial.suggest_int   ("n_neighbors",        10, 80)
+    n_components      = trial.suggest_int   ("n_components",        5, 30)
+    min_dist          = trial.suggest_float ("min_dist",            0.0, 0.5)
+    min_cluster_size  = trial.suggest_int   ("min_cluster_size",    4, 20)
+    min_samples       = trial.suggest_int   ("min_samples",         1, 5)
+    cluster_epsilon   = trial.suggest_float ("cluster_epsilon",     0.0, 0.20)
+    min_topic_size    = trial.suggest_int   ("min_topic_size",      4, 20)
+    threshold         = trial.suggest_float ("reassign_threshold",  0.05, 0.25)
 
+    n_outliers = fit_clusters(
+        n_neighbors, n_components, min_dist,
+        min_cluster_size, min_samples, cluster_epsilon,
+        min_topic_size, top_n_words=10,
+        nr_bins=30, threshold=threshold
+    )
+    return n_outliers
+
+def sweep():
+    # Bayesian parameter sweep
+    sampler = optuna.samplers.TPESampler(seed=42, multivariate=True)  # Bayesian TPE
+    study = optuna.create_study(direction="minimize",
+                                sampler=sampler,
+                                pruner=optuna.pruners.MedianPruner(n_warmup_steps=10))
+    study.optimize(objective, n_trials=100, timeout=8 * 60 * 60)
+
+# if __name__ == '__main__':
+    # {'n_neighbors': 38, 'n_components': 19, 'min_dist': 0.26222776670842407, 'min_c
+    # luster_size': 4, 'min_samples': 1, 'cluster_epsilon': 0.19291905969822112, 'min_
+    # topic_size': 16, 'reassign_threshold': 0.11078554780289114}
